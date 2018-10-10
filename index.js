4417bd81b417543463fdd344cdad9bb140690607
Samuel Ellis <pleasecastsamellis@gmail.com>
2018-10-10 01:21:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key;
-  return object;
 }
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key] : value } );
